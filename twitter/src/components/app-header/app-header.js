b1b10ee17c65f77b6815c31969a04ab7c7735dae
sergeevem2007@gmail.com <sergeevem2007@gmail.com>
2020-05-13 21:48:53 +0400

    feat:add files js in structure,add style

---
+++ twitter/src/components/app-header/app-header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './app-header.css';
+
+const AppHeader = () => {
+  return (
+    <div className="app-header d-flex">
+      <h1>Eugene Sergeev</h1>
+      <h2>5 записей, из них понравилось 1</h2>
+    </div>
+  )
+}
+
+export default AppHeader;
