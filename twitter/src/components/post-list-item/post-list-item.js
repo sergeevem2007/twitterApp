78d441ebe9ce8ac239516e715099122928011e36
sergeevem2007@gmail.com <sergeevem2007@gmail.com>
2020-06-30 00:43:22 +0400

    feat:add some func in app.js

--- twitter/src/components/post-list-item/post-list-item.js
+++ twitter/src/components/post-list-item/post-list-item.js
@@ -2,31 +2,9 @@ import React, {Component}  from 'react';
 import './post-list-item.css';
 
 export default class PostListItem extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      important: false,
-      like: false
-    };
-    this.onImportant = this.onImportant.bind(this);
-    this.onLike = this.onLike.bind(this);
-  }
-
-  onImportant() {
-    this.setState(({important})=>({
-      important: !important
-    }))
-  }
-
-  onLike() {
-    this.setState(({like})=>({
-      like: !like
-    }))
-  }
-
+ 
   render() {
-    const {label} = this.props;
-    const {important, like} = this.state;
+    const {label, important, like, onDelete, onToggleImportant, onToggleLiked} = this.props;
     let classNames = "app-list-item d-flex justify-content-between";
       if (important) {
         classNames += " important";
@@ -38,17 +16,20 @@ export default class PostListItem extends Component {
       <div className={classNames}>
       <span 
       className="app-list-item-label"
-      onClick={this.onLike}>
+      onClick={onToggleLiked}>
         {label}
       </span>
       <div className="d-flex justify-content-center align-items-center">
         <button 
         type="button" 
         className="btn-star btn-sm"
-        onClick={this.onImportant}>
+        onClick={onToggleImportant}>
           <i className="fa fa-star"></i>
         </button>
-        <button type="button" className="btn-trash btn-sm">
+        <button
+        type="button"
+        className="btn-trash btn-sm"
+        onClick={onDelete}>
           <i className="fa fa-trash-o"></i>
         </button>
         <i className="fa fa-heart"></i>
